36a428966af3bdcbde8f6e580b7a661193d97bcd
wizhi <xwizhi@gmail.com>
2015-11-17 21:09:41 +0100

    Updated XmlReportParser, so it actually parses the file and assigns properties.

--- Raunstrup/Raunstrup.Core/Xml/XmlReportParser.cs
+++ Raunstrup/Raunstrup.Core/Xml/XmlReportParser.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using System.Xml;
+using System.Xml.Schema;
 using Raunstrup.Core.Domain;
 using Raunstrup.Core.Repositories;
 
@@ -11,22 +13,28 @@ namespace Raunstrup.Core.Xml
         private readonly IProjectRepository _projectRepository;
         private readonly IEmployeeRepository _employeeRepository;
         private readonly IProductRepository _productRepository;
+        private readonly XmlReaderSettings _settings;
 
         public XmlReportParser(IProjectRepository projectRepository, IEmployeeRepository employeeRepository, IProductRepository productRepository)
         {
             _projectRepository = projectRepository;
             _employeeRepository = employeeRepository;
             _productRepository = productRepository;
+
+            _settings = new XmlReaderSettings { ValidationType = ValidationType.Schema };
+            _settings.Schemas.Add(
+                XmlSchema.Read(
+                    Assembly
+                        .GetExecutingAssembly()
+                        .GetManifestResourceStream("Raunstrup.Core.Xml.report.xsd"), 
+                    ValidationEventHandler
+                )
+            );
         }
 
         public Report Parse(string xml)
         {
-            var settings = new XmlReaderSettings();
-
-            settings.Schemas.Add(@"http://raunstrup.com/EAL", @"Xml/report.xsd");
-            settings.ValidationType = ValidationType.Schema;
-
-            var reader = XmlReader.Create(new StringReader(xml), settings);
+            var reader = XmlReader.Create(new StringReader(xml), _settings);
             var document = new XmlDocument();
             
             document.Load(reader);
@@ -35,18 +43,24 @@ namespace Raunstrup.Core.Xml
             var report = new Report(
                 _employeeRepository.Get(Convert.ToInt32(rootNode.Attributes["employeeId"].Value)), 
                 _projectRepository.Get(Convert.ToInt32(rootNode.Attributes["projectId"].Value))
-            );
+            ) { Date = DateTime.Parse(rootNode["date"].InnerText) };
 
-            foreach (XmlNode node in document.SelectNodes(@"\report\materials"))
+            foreach (XmlNode node in rootNode["materials"].ChildNodes)
             {
                 // TODO: Find a better way to do this, wtf.
                 report.AddReportLine(
-                    _productRepository.Get(Convert.ToInt32(node.Value)),
-                    Convert.ToInt32(node.Attributes["quantity"])
+                    _productRepository.Get(Convert.ToInt32(node.InnerText)),
+                    Convert.ToInt32(node.Attributes["quantity"].Value)
                 );
             }
 
             return report;
+        }
+
+        private void ValidationEventHandler(object sender, ValidationEventArgs validationEventArgs)
+        {
+            // TODO: Implement validation error handling.
+            throw new NotImplementedException();
         }
     }
 }
