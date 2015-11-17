a7bdbde92a579ac0c110c30ffdbfee92dd3795e7
Tobias Hesselberg Bechmann <tobi8719@edu.eal.dk>
2015-11-17 11:25:11 +0100

    Small changes

--- Raunstrup/Test/Program.cs
+++ Raunstrup/Test/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Raunstrup.Core;
 using Raunstrup.Core.Domain;
+using Raunstrup.Core.Repos;
 using Raunstrup.Core.statistics;
 
 namespace Test
@@ -12,26 +14,31 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            TestProjectComparison();
+            Console.ReadKey();
+        }
+
+        private static void TestProjectComparison()
+        {
             LineItem glas = new Material("glas");
             LineItem tree = new Material("træ");
             LineItem croc = new Material("krokodille");
             Project proc1 = new Project(new Draft());
-            proc1.GetDraft().addOrderLine(glas, 3);
-            proc1.GetDraft().addOrderLine(tree, 27);
-            proc1.GetDraft().addOrderLine(croc, 3);
-            proc1.GetDraft().addOrderLine(glas, 6);
+            proc1.GetDraft().AddOrderLine(glas, 3);
+            proc1.GetDraft().AddOrderLine(tree, 27);
+            proc1.GetDraft().AddOrderLine(croc, 3);
+            proc1.GetDraft().AddOrderLine(glas, 6);
             Report report1 = new Report();
-            report1.addReportLine(glas, 3);
-            report1.addReportLine(glas, 1);
-            report1.addReportLine(tree, 20);
+            report1.AddReportLine(glas, 3);
+            report1.AddReportLine(glas, 1);
+            report1.AddReportLine(tree, 20);
             Report report2 = new Report();
-            report2.addReportLine(tree, 7);
+            report2.AddReportLine(tree, 7);
             List<Report> reports = new List<Report>();
             reports.Add(report1);
             reports.Add(report2);
-            ProjectComparison comparison = new ProjectComparison(proc1, reports);
-            comparison.print();
-            Console.ReadKey();
+            ProjectComparison comparison = new ProjectComparison(proc1, new ReportRepo());
+            comparison.Print();
         }
     }
 }
