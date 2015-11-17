62c9b8be33f63bd3a10a4c0a2712b4ed3956f012
wizhi <xwizhi@gmail.com>
2015-11-17 15:52:49 +0100

    Implemented a naive version of UploadReport().
    No, I haven't tested it. Am I a good team member or what?

--- Raunstrup/Raunstrup.Core/Controllers/ReportController.cs
+++ Raunstrup/Raunstrup.Core/Controllers/ReportController.cs
@@ -1,20 +1,51 @@
-﻿using Raunstrup.Core.Domain;
+﻿using System;
+using System.IO;
+using Raunstrup.Core.Domain;
 using Raunstrup.Core.Repositories;
 using Raunstrup.Core.statistics;
+using Raunstrup.Core.Xml;
 
 namespace Raunstrup.Core.Controllers
 {
     public class ReportController
     {
+        // TODO: Consider Inversion of Control instead of all these.
         private readonly IReportRepository _reportRepository;
+        private readonly IProjectRepository _projectRepository;
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IProductRepository _productRepository;
 
-        public ReportController(IReportRepository reportRepository)
+        public ReportController(IReportRepository reportRepository, IProjectRepository projectRepository, IEmployeeRepository employeeRepository, IProductRepository productRepository)
         {
             _reportRepository = reportRepository;
+            _projectRepository = projectRepository;
+            _employeeRepository = employeeRepository;
+            _productRepository = productRepository;
         }
+
         public ProjectComparison GetProjectComparison(Project project)
         {
             return new ProjectComparison(project, _reportRepository);
+        }
+
+        public void UploadReport(string path)
+        {
+            try
+            {
+                var xml = File.ReadAllText(path);
+                var parser = new XmlReportParser(_projectRepository, _employeeRepository, _productRepository);
+                var report = parser.Parse(xml);
+
+                _reportRepository.Save(report);
+            }
+            catch (FileNotFoundException)
+            {
+                // TODO: Handle file not found.
+            }
+            catch (Exception)
+            {
+                // TODO: Handle all the other damn exceptions.
+            }
         }
     }
 }
