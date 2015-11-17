57b84b03223afdfb04d4a031c709313bae378437
wizhi <xwizhi@gmail.com>
2015-11-17 21:09:02 +0100

    Added Company class. I suppose this is sort of a IoC container?

---
+++ Raunstrup/Raunstrup.Core/Company.cs
@@ -0,0 +1,24 @@
+﻿using Raunstrup.Core.Repositories;
+
+namespace Raunstrup.Core
+{
+    public class Company
+    {
+        public IDraftRepository DraftRepository { get; private set; }
+        public IProjectRepository ProjectRepository { get; private set; }
+        public ICustomerRepository CustomerRepository { get; private set; }
+        public IReportRepository ReportRepository { get; private set; }
+        public IEmployeeRepository EmployeeRepository { get; private set; }
+        public IProductRepository ProductRepository { get; private set; }
+
+        public Company()
+        {
+            DraftRepository = new DraftRepository();
+            ProjectRepository = new ProjectRepository();
+            CustomerRepository = new CustomerRepository();
+            ReportRepository = new ReportRepository();
+            EmployeeRepository = new EmployeeRepository();
+            ProductRepository = new ProductRepository();
+        }
+    }
+}
