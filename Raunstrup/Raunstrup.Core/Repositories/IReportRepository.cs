c38c8e6130e6a1de97d11084701f4a7d79869ca3
wizhi <xwizhi@gmail.com>
2015-11-17 15:51:53 +0100

    Update IReportRepository so it actually includes FindByProject().
    Stupid Visual Studio.

--- Raunstrup/Raunstrup.Core/Repositories/IReportRepository.cs
+++ Raunstrup/Raunstrup.Core/Repositories/IReportRepository.cs
@@ -1,8 +1,10 @@
-﻿using Raunstrup.Core.Domain;
+﻿using System.Collections.Generic;
+using Raunstrup.Core.Domain;
 
 namespace Raunstrup.Core.Repositories
 {
     public interface IReportRepository : IRepository<Report>
     {
+        IList<Report> FindByProject(Project project);
     }
 }
