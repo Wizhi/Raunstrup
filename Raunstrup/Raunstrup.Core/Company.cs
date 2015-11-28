b90cfd6fe1f3e26b32e0f63d6e1b875aaee0f0fa
Tobias Hesselberg Bechmann <tobi8719@edu.eal.dk>
2015-11-28 18:05:32 +0100

    Made testdata work again, though everything have to be tested through company and with controllers now

--- Raunstrup/Raunstrup.Core/Company.cs
+++ Raunstrup/Raunstrup.Core/Company.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Specialized;
 using Raunstrup.Core.Controllers;
 using Raunstrup.Core.Repositories;
+using Raunstrup.Domain;
 
 namespace Raunstrup.Core
 {
@@ -23,6 +24,7 @@ namespace Raunstrup.Core
             ReportRepository = new ReportRepository();
             EmployeeRepository = new EmployeeRepository();
             ProductRepository = new ProductRepository();
+            SetupTestData();
         }
 
         public ReportController GetReportController()
@@ -34,5 +36,162 @@ namespace Raunstrup.Core
         {
             return new DraftController(CustomerRepository,EmployeeRepository,ProductRepository,DraftRepository);
         }
+
+        //All this test data should be deleted at some point, but for now it will have to be here
+        private void SetupTestData()
+        {
+            SetupProducts();
+            SetupCustomers();
+            SetupEmployees();
+            SetupDrafts();
+            SetupProjects();
+            SetupReports();
+        }
+
+        private void SetupProducts()
+        {
+            var products = new[]
+            {
+                new Product { Name = "Foo", SalesPrice = 123.4M }, 
+                new Product { Name = "BarBarBarBarBarBarBarBarBar", SalesPrice = 200M }, 
+                new Product { Name = "Baz", SalesPrice = 321.2M }, 
+            };
+
+            foreach (var product in products)
+            {
+                ProductRepository.Save(product);
+            }
+        }
+
+        private void SetupCustomers()
+        {
+            var customers = new[]
+            {
+                new Customer { Name = "Kunde #1" },
+                new Customer { Name = "Kunde #2" },
+            };
+
+            foreach (var customer in customers)
+            {
+                CustomerRepository.Save(customer);
+            }
+        }
+
+        private void SetupEmployees()
+        {
+            var employees = new[]
+            {
+                new Employee { Name = "Emil" },
+                new Employee { Name = "Tobias" },
+                new Employee { Name = "Simon" },
+            };
+
+            foreach (var employee in employees)
+            {
+                EmployeeRepository.Save(employee);
+            }
+        }
+
+        private void SetupDrafts()
+        {
+            var drafts = new[]
+            {
+                new Draft(CustomerRepository.Get(1))
+                {
+                    Title = "Draft #1", 
+                    ResponsiblEmployee = EmployeeRepository.Get(1)
+                },
+                new Draft(CustomerRepository.Get(1))
+                {
+                    Title = "Draft #2", 
+                    ResponsiblEmployee = EmployeeRepository.Get(2)
+                },
+                new Draft(CustomerRepository.Get(2))
+                {
+                    Title = "Draft #3", 
+                    ResponsiblEmployee = EmployeeRepository.Get(3)
+                },
+                new Draft(CustomerRepository.Get(2))
+                {
+                    Title = "Draft #4", 
+                    ResponsiblEmployee = EmployeeRepository.Get(1)
+                },
+            };
+
+            drafts[0].AddOrderLine(ProductRepository.Get(1), 2);
+            drafts[0].AddOrderLine(ProductRepository.Get(2), 3);
+            drafts[0].AddOrderLine(ProductRepository.Get(3), 1);
+
+            drafts[1].AddOrderLine(ProductRepository.Get(1), 5);
+
+            drafts[2].AddOrderLine(ProductRepository.Get(1), 3);
+            drafts[2].AddOrderLine(ProductRepository.Get(2), 6);
+
+            drafts[0].AddOrderLine(ProductRepository.Get(1), 1);
+            drafts[0].AddOrderLine(ProductRepository.Get(2), 6);
+            drafts[0].AddOrderLine(ProductRepository.Get(3), 3);
+
+            foreach (var draft in drafts)
+            {
+                DraftRepository.Save(draft);
+            }
+        }
+
+        private void SetupProjects()
+        {
+            var projects = new[]
+            {
+                new Project(DraftRepository.Get(1)), 
+                new Project(DraftRepository.Get(2)), 
+                new Project(DraftRepository.Get(3)), 
+            };
+
+            foreach (var project in projects)
+            {
+                ProjectRepository.Save(project);
+            }
+        }
+
+        private void SetupReports()
+        {
+            var reports = new[]
+            {
+                new Report(EmployeeRepository.Get(1), ProjectRepository.Get(1))
+                {
+                    Date = ProjectRepository.Get(1).OrderDate.AddDays(1)
+                },
+                new Report(EmployeeRepository.Get(2), ProjectRepository.Get(1))
+                {
+                    Date = ProjectRepository.Get(1).OrderDate.AddDays(2)
+                },
+                new Report(EmployeeRepository.Get(2), ProjectRepository.Get(2))
+                {
+                    Date = ProjectRepository.Get(2).OrderDate.AddDays(2)
+                },
+                new Report(EmployeeRepository.Get(3), ProjectRepository.Get(2))
+                {
+                    Date = ProjectRepository.Get(2).OrderDate.AddDays(4)
+                },
+            };
+
+            reports[0].AddReportLine(ProductRepository.Get(1), 4);
+            reports[0].AddReportLine(ProductRepository.Get(3), 2);
+            reports[0].AddReportLine(ProductRepository.Get(2), 1);
+
+            reports[1].AddReportLine(ProductRepository.Get(3), 4);
+            reports[1].AddReportLine(ProductRepository.Get(2), 6);
+
+            reports[2].AddReportLine(ProductRepository.Get(3), 7);
+
+            reports[3].AddReportLine(ProductRepository.Get(2), 2);
+            reports[3].AddReportLine(ProductRepository.Get(3), 4);
+            reports[3].AddReportLine(ProductRepository.Get(1), 7);
+
+            foreach (var report in reports)
+            {
+                ReportRepository.Save(report);
+            }
+        }
+        //This should be deltede sometime
     }
 }
