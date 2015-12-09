eb84271a93a78fbfc3d87f8e6db6413be38bc18f
Simon Bo Dall Mikkelsen <simo451g@edu.eal.dk>
2015-12-09 22:50:35 +0100

    Implemented a shitty version of the Query Object pattern.

---
+++ Raunstrup/Raunstrup.Data.MsSql/Query/IQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace Raunstrup.Data.MsSql.Query
+{
+    interface IQuery<T>
+    {
+        T Execute(DataContext context);
+    }
+}
---
+++ Raunstrup/Raunstrup.Data.MsSql/Query/ReportByProjectQuery.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Raunstrup.Data.MsSql.Mappers;
+using Raunstrup.Domain;
+
+namespace Raunstrup.Data.MsSql.Query
+{
+    class ReportByProjectQuery : IQuery<IList<Report>>
+    {
+        private readonly Project _project;
+        private readonly ReportMapper _mapper;
+
+        public ReportByProjectQuery(Project project)
+        {
+            _project = project;
+        }
+
+        public IList<Report> Execute(DataContext context)
+        {
+            using (var connection = context.CreateConnection())
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = @"SELECT * FROM Project WHERE ProductId = @projectId";
+
+                var projectIdParam = command.CreateParameter();
+
+                projectIdParam.ParameterName = "@projectId";
+                projectIdParam.Value = _project.Id;
+                projectIdParam.DbType = DbType.Int32;
+
+                command.Parameters.Add(projectIdParam);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    return new ReportMapper(context).MapAll(reader);
+                }
+            }
+        }
+    }
+}
