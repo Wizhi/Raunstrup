2639c0b6f8333ef222b1c5e88b749f59af5f16b3
Simon Bo Dall Mikkelsen <simo451g@edu.eal.dk>
2015-12-10 00:30:41 +0100

    Added ReadOnlyMaterial.

---
+++ Raunstrup/Raunstrup.Domain/ViewObjects/ReadOnlyMaterial.cs
@@ -0,0 +1,15 @@
+﻿namespace Raunstrup.Domain.ViewObjects
+{
+    class ReadOnlyMaterial : ReadOnlyProduct
+    {
+        private readonly Material _material;
+
+        public ReadOnlyMaterial(Material material) 
+            : base(material)
+        {
+            _material = material;
+        }
+
+        public decimal CostProce { get { return _material.CostPrice; } }
+    }
+}
