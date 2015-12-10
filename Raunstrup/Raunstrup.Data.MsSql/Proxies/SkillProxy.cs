6ef9065f00558e0e89018991e9f1a035ebe91731
Simon Bo Dall Mikkelsen <simo451g@edu.eal.dk>
2015-12-11 00:49:49 +0100

    Added SkillProxy.

---
+++ Raunstrup/Raunstrup.Data.MsSql/Proxies/SkillProxy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Raunstrup.Data.MsSql.Mappers;
+using Raunstrup.Domain;
+
+namespace Raunstrup.Data.MsSql.Proxies
+{
+    class SkillProxy : Skill
+    {
+        private readonly Lazy<Skill> _real;
+
+        public SkillProxy(DataContext context, int id)
+        {
+            _real = new Lazy<Skill>(() => new SkillMapper(context).Get(id));
+        }
+
+        public int Id
+        {
+            get { return _real.Value.Id; }
+            set { _real.Value.Id = value; }
+        }
+
+        public string Name
+        {
+            get { return _real.Value.Name; }
+            set { _real.Value.Name = value; }
+        }
+    }
+}
