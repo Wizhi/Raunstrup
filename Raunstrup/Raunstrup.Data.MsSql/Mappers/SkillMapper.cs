0114c6954787ac3785fea30c8c41816247706198
Simon Bo Dall Mikkelsen <simo451g@edu.eal.dk>
2015-12-10 22:02:31 +0100

    Added insert, update, getall, mapping, whatever.
    I sure as fuck hope it works.

--- Raunstrup/Raunstrup.Data.MsSql/Mappers/SkillMapper.cs
+++ Raunstrup/Raunstrup.Data.MsSql/Mappers/SkillMapper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using Raunstrup.Domain;
 
@@ -38,6 +39,63 @@ namespace Raunstrup.Data.MsSql.Mappers
             }
         }
 
+        public IList<Skill> GetAll()
+        {
+            using (var connection = _context.CreateConnection())
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT Id, Name FROM Skill";
+                
+                connection.Open();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    return MapAll(reader);
+                }
+            }
+        }
+
+        public void Insert(Skill skill)
+        {
+            using (var connection = _context.CreateConnection())
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = @"INSERT INTO Skill (Name) VALUES (@name);
+                                        SELECT SCOPE_IDENTITY();";
+
+                SetParameters(command, skill);
+
+                connection.Open();
+                command.Prepare();
+
+                skill.Id = (int) command.ExecuteScalar();
+            }
+        }
+
+        public void Update(Skill skill)
+        {
+            using (var connection = _context.CreateConnection())
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = @"UPDATE Skill SET Name=@name WHERE SkillId=@id";
+
+                var idParam = command.CreateParameter();
+
+                idParam.ParameterName = "@id";
+                idParam.Value = skill.Id;
+                idParam.DbType = DbType.Int32;
+
+                command.Parameters.Add(idParam);
+
+                SetParameters(command, skill);
+
+                connection.Open();
+                command.Prepare();
+
+                skill.Id = (int)command.ExecuteScalar();
+            }
+        }
+
         public Skill Map(IDataReader reader)
         {
             Skill skill = null;
@@ -52,6 +110,31 @@ namespace Raunstrup.Data.MsSql.Mappers
             }
 
             return skill;
+        }
+
+        public IList<Skill> MapAll(IDataReader reader)
+        {
+            var skills = new List<Skill>();
+            Skill skill;
+
+            while ((skill = Map(reader)) != null)
+            {
+                skills.Add(skill);
+            }
+
+            return skills;
+        }
+
+        private void SetParameters(IDbCommand command, Skill skill)
+        {
+            var nameParam = command.CreateParameter();
+
+            nameParam.ParameterName = "@name";
+            nameParam.Value = skill.Name;
+            nameParam.DbType = DbType.AnsiString;
+            nameParam.Size = 100;
+
+            command.Parameters.Add(nameParam);
         }
     }
 }
