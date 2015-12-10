896e1ed4c7fc028adc4f6a3941fc9fab2502b17a
Recyfer <emilmylius@gmail.com>
2015-12-10 23:05:06 +0100

    Added CostPrice option

--- Raunstrup/Raunstrup.Forms/ProductCRUDForm.cs
+++ Raunstrup/Raunstrup.Forms/ProductCRUDForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Raunstrup.Core;
@@ -20,7 +21,8 @@ namespace Raunstrup.Forms
         public ProductCRUDForm(Company company)
         {
             InitializeComponent();
-            _productPriceNumericUpDown.Maximum = Decimal.MaxValue;
+            _productPriceNumericUpDown.Maximum = decimal.MaxValue;
+            _costPriceNumericUpDown.Maximum = decimal.MaxValue;
             _editMode = false;
             _productController = company.GetProductController();
             _materialRadioButton.Checked = true;
@@ -29,7 +31,8 @@ namespace Raunstrup.Forms
         public ProductCRUDForm(Company company, ReadOnlyProduct product)
         {
             InitializeComponent();
-            _productPriceNumericUpDown.Maximum = Decimal.MaxValue;
+            _productPriceNumericUpDown.Maximum = decimal.MaxValue;
+            _costPriceNumericUpDown.Maximum = decimal.MaxValue;
             _productNameTextBox.Text = product.Name;
             _productPriceNumericUpDown.Value = product.SalesPrice;
             if (product is ReadOnlyMaterial)
@@ -39,10 +42,14 @@ namespace Raunstrup.Forms
             else if (product is ReadOnlyWorkHour)
             {
                 _workHourRadioButton.Checked = true;
+                _costPriceNumericUpDown.Visible = false;
+                _costPriceLabel.Visible = false;
             }
             else if (product is ReadOnlyTransport)
             {
                 _transportRadioButton.Checked = true;
+                _costPriceNumericUpDown.Visible = false;
+                _costPriceLabel.Visible = false;
             }
             _materialRadioButton.Enabled = false;
             _workHourRadioButton.Enabled = false;
@@ -54,32 +61,76 @@ namespace Raunstrup.Forms
 
         private void _saveProductButton_Click(object sender, EventArgs e)
         {
-            if (_editMode)
+            try
             {
-                _productController.SetName(_productNameTextBox.Text);
-                _productController.SetPrice(_productPriceNumericUpDown.Value);
-                _productController.SaveProduct();
+                if (_editMode)
+                {
+                    _productController.SetName(_productNameTextBox.Text);
+                    _productController.SetPrice(_productPriceNumericUpDown.Value);
+                    _productController.SaveProduct();
+                }
+                else if (!_editMode)
+                {
+                    if (_materialRadioButton.Checked)
+                    {
+                        _productController.CreateNewProduct(ProductCRUDController.ProductType.Material);
+                        _productController.SetCostPrice(_costPriceNumericUpDown.Value);
+                    }
+                    else if (_workHourRadioButton.Checked)
+                    {
+                        _productController.CreateNewProduct(ProductCRUDController.ProductType.WorkHour);
+                    }
+                    else if (_transportRadioButton.Checked)
+                    {
+                        _productController.CreateNewProduct(ProductCRUDController.ProductType.Transport);
+                    }
+                    _productController.SetName(_productNameTextBox.Text);
+                    _productController.SetPrice(_productPriceNumericUpDown.Value);
+                    _productController.SaveProduct();
+                }
+                MessageBox.Show(@"Varen blev gemt.");
             }
-            else if (!_editMode)
+            catch(Exception exception)
             {
-                if (_materialRadioButton.Checked)
-                {
-                    _productController.CreateNewProduct(ProductCRUDController.ProductType.Material);
-                }
-                else if (_workHourRadioButton.Checked)
-                {
-                    _productController.CreateNewProduct(ProductCRUDController.ProductType.WorkHour);
-                }
-                else if (_transportRadioButton.Checked)
-                {
-                    _productController.CreateNewProduct(ProductCRUDController.ProductType.Transport);
-                }
-                _productController.SetName(_productNameTextBox.Text);
-                _productController.SetPrice(_productPriceNumericUpDown.Value);
-                _productController.SaveProduct();
-            
+                MessageBox.Show(exception.ToString());
             }
-            
+        }
+
+        private void _materialRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (_materialRadioButton.Checked)
+            {
+                MoveDown();
+            }
+            else if (!_materialRadioButton.Checked)
+            {
+                MoveUp();
+            }
+
+        }
+
+        private void MoveDown()
+        {
+            Height += 28;
+            _transportRadioButton.Location = new Point(_transportRadioButton.Location.X, _transportRadioButton.Location.Y + 28);
+            _workHourRadioButton.Location = new Point(_workHourRadioButton.Location.X, _workHourRadioButton.Location.Y + 28);
+            _materialRadioButton.Location = new Point(_materialRadioButton.Location.X, _materialRadioButton.Location.Y + 28);
+            _saveProductButton.Location = new Point(_saveProductButton.Location.X, _saveProductButton.Location.Y + 28);
+            _costPriceLabel.Visible = true;
+            _costPriceNumericUpDown.Enabled = true;
+            _costPriceNumericUpDown.Visible = true;
+        }
+
+        private void MoveUp()
+        {
+            _costPriceLabel.Visible = false;
+            _costPriceNumericUpDown.Enabled = false;
+            _costPriceNumericUpDown.Visible = false;
+            Height -= 28;
+            _saveProductButton.Location = new Point(_saveProductButton.Location.X, _saveProductButton.Location.Y - 28);
+            _materialRadioButton.Location = new Point(_materialRadioButton.Location.X, _materialRadioButton.Location.Y - 28);
+            _workHourRadioButton.Location = new Point(_workHourRadioButton.Location.X, _workHourRadioButton.Location.Y - 28);
+            _transportRadioButton.Location = new Point(_transportRadioButton.Location.X, _transportRadioButton.Location.Y - 28);
         }
     }
 }
