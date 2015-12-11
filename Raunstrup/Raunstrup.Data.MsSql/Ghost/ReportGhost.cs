c35023035ff78136b955130d1cbbab623e160b61
Simon Bo Dall Mikkelsen <simo451g@edu.eal.dk>
2015-12-11 20:57:58 +0100

    Sheez..why wasn't this in version control.

---
+++ Raunstrup/Raunstrup.Data.MsSql/Ghost/ReportGhost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Raunstrup.Domain;
+
+namespace Raunstrup.Data.MsSql.Ghost
+{
+    class ReportGhost : Report
+    {
+        private readonly Lazy<IList<ReportLine>> _reportLines;
+
+        public ReportGhost(Employee employee, Project project, Func<IList<ReportLine>> reportLines)
+            : base(employee, project)
+        {
+            _reportLines = new Lazy<IList<ReportLine>>(reportLines);
+        }
+
+        public override IList<ReportLine> Lines { get { return _reportLines.Value; } }
+    }
+}
