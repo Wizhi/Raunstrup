40ba15cc9c73365a75fe5f5ecbce69b09cacbc0b
Recyfer <emilmylius@gmail.com>
2015-12-11 01:29:42 +0100

    Added ReadOnlySkill

---
+++ Raunstrup/Raunstrup.Domain/ViewObjects/ReadOnlySkill.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raunstrup.Domain.ViewObjects
+{
+    class ReadOnlySkill
+    {
+        private readonly Skill _real;
+
+        public ReadOnlySkill(Skill real)
+        {
+            _real = real;
+        }
+
+        public int Id
+        {
+            get { return _real.Id; }
+        }
+
+        public string Name
+        {
+            get { return _real.Name; }
+        }
+    }
+}
