529f2b87b545ccf76cef6041bc146a79cf340b3e
Recyfer <emilmylius@gmail.com>
2015-12-11 15:23:02 +0100

    Added SkillCRUDForm and updated proejct file

---
+++ Raunstrup/Raunstrup.Forms/SkillCRUDForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Raunstrup.Core;
+using Raunstrup.Core.Controllers;
+using Raunstrup.Domain.ViewObjects;
+
+namespace Raunstrup.Forms
+{
+    public partial class SkillCRUDForm : Form
+    {
+        private EmployeeCRUDController _employeeCRUDController;
+        public SkillCRUDForm(Company company)
+        {
+            InitializeComponent();
+            _employeeCRUDController = company.GetEmployeeCRUDController();
+            _employeeCRUDController.CreateNewSkill();
+        }
+
+        public SkillCRUDForm(Company company, ReadOnlySkill skill)
+        {
+            InitializeComponent();
+            _employeeCRUDController = company.GetEmployeeCRUDController();
+            _employeeCRUDController.EditSkill(skill.Id);
+            _skillNameTextBox.Text = skill.Name;
+        }
+
+        private void _saveSkillButton_Click(object sender, EventArgs e)
+        {
+            _employeeCRUDController.SetSkillName(_skillNameTextBox.Text);
+            _employeeCRUDController.SaveSkill();
+            MessageBox.Show(@"Specialet blev gemt.");
+            Close();
+        }
+    }
+}
