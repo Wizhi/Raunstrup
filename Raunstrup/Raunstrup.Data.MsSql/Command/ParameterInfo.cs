14c36e8a041814b071b45bae4829e35f359f79a1
Simon Bo Dall Mikkelsen <simo451g@edu.eal.dk>
2015-12-12 06:31:00 +0100

    Seriously VS, why didn't you commit this?
    I'm just glad we don't really care about version history.

---
+++ Raunstrup/Raunstrup.Data.MsSql/Command/ParameterInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+
+namespace Raunstrup.Data.MsSql.Command
+{
+    class ParameterInfo
+    {
+        public DbType DbType { get; set; }
+        public int Size { get; set; }
+        public byte Precision { get; set; }
+        public byte Scale { get; set; }
+
+        public IDbDataParameter ToParameter(Func<IDbDataParameter> factory)
+        {
+            var parameter = factory();
+
+            parameter.DbType = DbType;
+            parameter.Size = Size;
+            parameter.Precision = Precision;
+            parameter.Scale = Scale;
+
+            return parameter;
+        }
+    }
+}
