1472351725607f88029d8dd056e5eab81071df70
Simon Bo Dall Mikkelsen <simo451g@edu.eal.dk>
2015-12-12 08:15:22 +0100

    Refactoring and documentation.

--- Raunstrup/Raunstrup.Data.MsSql/DataContext.cs
+++ Raunstrup/Raunstrup.Data.MsSql/DataContext.cs
@@ -1,20 +1,32 @@
-﻿using System;
-using System.Data;
+﻿using System.Data;
 using System.Data.Common;
 
 namespace Raunstrup.Data.MsSql
 {
+    /// <summary>
+    /// The context used for interacting with the database.
+    /// </summary>
+    /// <remarks>Really it's just a wrapper around the <see cref="DbProviderFactory"/>..</remarks>
     public class DataContext
     {
         private readonly DbProviderFactory _providerFactory;
         private readonly string _connectionString;
 
+        /// <summary>
+        /// Creates a <see cref="DataContext"/>.
+        /// </summary>
+        /// <param name="providerFactory">The ADO.NET <see cref="DbProviderFactory"/> for the database type.</param>
+        /// <param name="connectionString">The connection string for the database.</param>
         public DataContext(DbProviderFactory providerFactory, string connectionString)
         {
             _providerFactory = providerFactory;
             _connectionString = connectionString;
         }
 
+        /// <summary>
+        /// Creates a new <see cref="IDbConnection"/> using the provider.
+        /// </summary>
+        /// <returns></returns>
         public IDbConnection CreateConnection()
         {
             var connection = _providerFactory.CreateConnection();
--- Raunstrup/Raunstrup.Data/Repositories/IRepository.cs
+++ Raunstrup/Raunstrup.Data/Repositories/IRepository.cs
@@ -1,8 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Raunstrup.Data.Repositories
 {
+    /// <summary>
+    /// A collection-like interface allowing for the buisness logic to stay ignorant of persistence logic.
+    /// </summary>
+    /// <remarks>
+    /// This isn't a true repository implementation as it lacks the querying capabilities of real repositories.
+    /// This was a planned feature, but turned out to be more of a challenge than expected.
+    /// </remarks>
+    /// <typeparam name="TEntity">The type of entity stored in the repository.</typeparam>
     public interface IRepository<TEntity>
     {
         TEntity Get(int id);
