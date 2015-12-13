373099260ba5195c614a65ac0663df93aba278cb
Simon Bo Dall Mikkelsen <simo451g@edu.eal.dk>
2015-12-13 19:35:34 +0100

    Added documentation.

--- Raunstrup/Raunstrup.Data.MsSql/Query/IQuery.cs
+++ Raunstrup/Raunstrup.Data.MsSql/Query/IQuery.cs
@@ -1,9 +1,20 @@
-﻿using System.Data;
-
-namespace Raunstrup.Data.MsSql.Query
+﻿namespace Raunstrup.Data.MsSql.Query
 {
+    /// <summary>
+    /// The base interface for all queries.
+    /// </summary>
+    /// <remarks>
+    /// Due to the "incomplete" implementation of the repository pattern, buisness logic related queries have been moved to individual classes.
+    /// It's kind of messy, but it's a lot cleaner.
+    /// </remarks>
+    /// <typeparam name="T">The type which <see cref="Execute"/> must return.</typeparam>
     interface IQuery<T>
     {
+        /// <summary>
+        /// Executes the query in the given context.
+        /// </summary>
+        /// <param name="context">The <see cref="DataContext"/> to execute the query in.</param>
+        /// <returns></returns>
         T Execute(DataContext context);
     }
 }
