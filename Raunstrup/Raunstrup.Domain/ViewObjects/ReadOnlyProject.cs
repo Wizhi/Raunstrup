b6775522027a50c2df2b5784e40167d96df7afdb
Recyfer <emilmylius@gmail.com>
2015-12-18 00:28:26 +0100

    Name refactored

--- Raunstrup/Raunstrup.Domain/ViewObjects/ReadOnlyProject.cs
+++ Raunstrup/Raunstrup.Domain/ViewObjects/ReadOnlyProject.cs
@@ -6,20 +6,20 @@ namespace Raunstrup.Domain.ViewObjects
 {
     public class ReadOnlyProject
     {
-        private readonly Project _real;
+        private readonly Project _project;
         private readonly Lazy<ReadOnlyDraft> _lazyDraft; 
         private readonly Lazy<IList<ReadOnlyEmployee>> _lazyEmployees; 
 
-        public ReadOnlyProject(Project real)
+        public ReadOnlyProject(Project project)
         {
-            _real = real;
-            _lazyDraft = new Lazy<ReadOnlyDraft>(() => new ReadOnlyDraft(_real.Draft));
-            _lazyEmployees = new Lazy<IList<ReadOnlyEmployee>>(() => _real.Employees.Select(employee => new ReadOnlyEmployee(employee)).ToList());
+            _project = project;
+            _lazyDraft = new Lazy<ReadOnlyDraft>(() => new ReadOnlyDraft(_project.Draft));
+            _lazyEmployees = new Lazy<IList<ReadOnlyEmployee>>(() => _project.Employees.Select(employee => new ReadOnlyEmployee(employee)).ToList());
         }
 
         public int Id
         {
-            get { return _real.Id; }
+            get { return _project.Id; }
         }
 
         public ReadOnlyDraft Draft
@@ -29,7 +29,7 @@ namespace Raunstrup.Domain.ViewObjects
 
         public DateTime OrderDate
         {
-            get { return _real.OrderDate; }
+            get { return _project.OrderDate; }
         }
 
         public IList<ReadOnlyEmployee> Employees
